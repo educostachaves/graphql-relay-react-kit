abee5d40e9d348baadf8ed0e361801a40ff0f4d2
Eduardo Chaves <educostachaves@hotmail.com>
2015-11-12 14:37:55 -0200

    Installing Setting Mongoose

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,14 @@
+import mongoose from 'mongoose';
 import express from 'express';
 import { Schema } from './data/schema';
 import graphQLHTTP from 'express-graphql';
 
+mongoose.connect(process.env.DB || 'mongodb://localhost/chimera-dev');
+
 const app = express();
-app.use('/', graphQLHTTP({ schema: Schema, pretty: true }));
+const port = process.env.PORT || 3000;
+
+app.use('/', graphQLHTTP({ schema: Schema, pretty: true, graphiql: true }));
 app.listen(9000, (err) => {
   if (err)
     return console.error(err);
